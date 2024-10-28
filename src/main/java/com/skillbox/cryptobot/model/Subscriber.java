4f9a962e054f68768821f03a26b1eb0a0becb881
Artem Sergienko <96276917+Ozsfag@users.noreply.github.com>
2024-10-28 21:08:14 +0900

    add subscribe class

---
+++ src/main/java/com/skillbox/cryptobot/model/Subscriber.java
@@ -0,0 +1,31 @@
+package com.skillbox.cryptobot.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "subscriber`",
+        schema = "crypto_bot"
+//        indexes = {
+////                @Index(name = "idx_lemma_id_page_id", columnList = "lemma_id, page_id"),
+////                @Index(name = "idx_rank_lemma_id_page_id", columnList = "rank,lemma_id, page_id")
+////        }
+ )
+@AllArgsConstructor
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Builder
+public class Subscriber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "uuid", columnDefinition = "INT")
+    private Integer uuid;
+
+    @Column(name = "tId", nullable = false, columnDefinition = "INT", unique = true)
+    private Integer tId;
+
+    @Column(columnDefinition = "TEXT")
+    private String price;
+}
